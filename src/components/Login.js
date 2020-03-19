22d87ff8fbafbb58d2c211da11c668db18c61fa9
shuji watanabe <rinkioutouhenmonzizai@yahoo.co.jp>
2020-03-19 16:19:24 +0900

    token authentication succeeded

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,13 +13,28 @@ export default class Login extends Component {
                 })
                 .then((res) => {
                     console.log(res)
+                    localStorage.setItem('token', res.data.key)
                 })
                 .catch(() => { console.log('login failed') })
+        }
+
+        const authentication = () => {
+            const URL = `${API_SERVER}/rest-auth/user/`
+            const TOKEN = 'da7fa020eca55cb80bdcbbca9575436da5f8cdbe'
+            axios
+                .get(URL, {
+                    headers: {
+                        AUthorization: `Token ${TOKEN}`,
+                    }
+                })
+                .then((res) => { console.log(res) })
+                .catch(() => { console.log('token authentication failed') })
         }
         return (
             <div>
                 <input></input>
                 <button onClick={login}>send</button>
+                <button onClick={authentication}>auth</button>
             </div>
         )
     }
